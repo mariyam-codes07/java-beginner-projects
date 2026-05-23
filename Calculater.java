a2ece75808e778df070cf880a5137fb9a255a8f0
Mariyam Mujawar <mariyammujawar77@gmail.com>
2026-05-23 21:26:36 +0530

    Update Calculater.java

--- Calculater.java
+++ Calculater.java
@@ -1,5 +1,6 @@
 import java.util.*;
-public class Calculater{
+public class Calculator{
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
      System.out.println("Enter a:");
